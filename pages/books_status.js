44b014e41de5bbbfb330c451ed853f4b6efe2664
Varun Sai <varunsai855@gmail.com>
2024-03-18 16:22:01 -0400

    03182024 commit 1

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,13 @@
 let BookInstance = require('../models/bookinstance');
+const Book = require("../models/book");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+function get_books () {
+  return BookInstance.find({'status': 'Available'}, 'title status').populate('book');
+}
+
+exports.show_all_books_status = async function(res) {
+  let books = await get_books().exec();
+  return res.send(books.map(function(b) {
+    return b.book.title + ' ' + b.status;
+  }));
 }
